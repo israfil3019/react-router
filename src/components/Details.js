fb457ad1639f17fc9f86eb832a7c86bb87977490
israfil <israfil3019@gmail.com>
2021-05-20 14:55:49 +0300

    show data with id to details page

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Details = (props) => {
+  const [post, setPost] = useState();
+
+  console.log({ props });
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${props.match.params.id}`)
+      .then((response) => response.json())
+      .then((data) => setPost(data));
+  }, []);
+
   return (
     <div>
-      <h1>This is -----detail------ page</h1>
-      <p>{props.match.params.id}</p>
+      <h2>Details {props.match.params.id} </h2>
+      <p>{post?.id}</p>
+      <p>{post?.title}</p>
+      <p>{post?.body}</p>
     </div>
   );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,9 @@
 import { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 function Home() {
   const [posts, setPosts] = useState();
+  const history = useHistory();
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
@@ -9,14 +11,21 @@ function Home() {
       .then((data) => setPosts(data));
   }, []);
 
+  const postHandler = (id) => {
+    console.log(id);
+    history.push(`details/${id}`);
+  };
+
   return (
     <div className="App">
       {posts?.map((post, index) => (
         <div
           key={index}
           style={{ border: "1px solid ", margin: 10, cursor: "pointer" }}
+          onClick={() => postHandler(post.id)}
         >
           <p>{post.title}</p>
+          <p>{post.id}</p>
         </div>
       ))}
     </div>
