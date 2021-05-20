f02dc73f623961954686e84674da5289f6495f03
israfil <israfil3019@gmail.com>
2021-05-20 15:07:39 +0300

    add button use history an location hooks

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,22 +1,33 @@
 import React, { useState, useEffect } from "react";
+import { useParams, useHistory, useLocation } from "react-router-dom";
 
 const Details = (props) => {
   const [post, setPost] = useState();
+  const { id } = useParams();
+  const history = useHistory();
+  const { pathname } = useLocation();
 
   console.log({ props });
+  console.log({ id });
+  console.log({ pathname });
 
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/posts/${props.match.params.id}`)
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
       .then((response) => response.json())
       .then((data) => setPost(data));
-  }, []);
+  }, [id]);
+
+  const goHome = (id) => {
+    history.push("/");
+  };
 
   return (
     <div>
-      <h2>Details {props.match.params.id} </h2>
+      <h2>Details {id} </h2>
       <p>{post?.id}</p>
       <p>{post?.title}</p>
       <p>{post?.body}</p>
+      <button onClick={goHome}>Go Home</button>
     </div>
   );
 };
